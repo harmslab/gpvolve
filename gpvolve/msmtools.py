3acec01faeef8d8d90270976dc5e577957fd6ca4
Zsailer <zachsailer@gmail.com>
2018-08-21 11:04:14 -0700

    remove old gpvolve

--- gpvolve/msmtools.py
+++
@@ -1,5 +0,0 @@
-# MSMTool utilities
-#
-#
-from msmtools.analysis.dense.pcca import PCCA
-from msmtools.flux.api import tpt as TPT
--- gpvolve/utils.py
+++
@@ -1,5 +0,0 @@
-
-
-def hamming_distance(s1, s2):
-    """Return the Hamming distance between equal-length sequences """
-    return sum(ch1 != ch2 for ch1, ch2 in zip(s1, s2))
