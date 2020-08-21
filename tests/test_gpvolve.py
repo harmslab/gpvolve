f63d7474247f4092f49146ea986b32519ac2cbb2
Luis Perez <lperezmo@uoregon.edu>
2020-08-21 16:44:28 -0700

    Added a few unit tests

--- tests/test_gpvolve.py
+++ tests/test_gpvolve.py
@@ -4,22 +4,61 @@
 """Tests for `gpvolve` package."""
 
 import pytest
+from gpvolve import GenotypePhenotypeMSM
+from gpmap import GenotypePhenotypeMap
+from gpgraph import GenotypePhenotypeGraph
+import numpy as np
 
 
-from gpvolve import gpvolve
+@pytest.fixture()
+def gpvolve_base():
+    wildtype = "AAA"
+
+    genotypes = [
+        "AAA",
+        "AAB",
+        "ABA",
+        "BAA",
+        "ABB",
+        "BAB",
+        "BBA",
+        "BBB"
+    ]
+
+    binary = [
+        '000',
+        '001',
+        '010',
+        '100',
+        '011',
+        '101',
+        '110',
+        '111'
+    ]
+
+    mutations = {
+        0: ["A", "B"],
+        1: ["A", "B"],
+        2: ["A", "B"],
+    }
+    phenotypes = np.random.rand(len(genotypes))
+    tmp_gpm_file = GenotypePhenotypeMap(wildtype=wildtype,
+                                        genotypes=genotypes,
+                                        phenotypes=phenotypes,
+                                        log_transform=False,
+                                        mutations=mutations)
+    gpv = GenotypePhenotypeMSM(tmp_gpm_file)
+    return gpv
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+def test_gpvolve(gpvolve_base):
+    """Test type of object"""
+    assert isinstance(gpvolve_base, GenotypePhenotypeMSM)
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_inner_gpmap(gpvolve_base):
+    assert isinstance(gpvolve_base.gpm, GenotypePhenotypeMap)
+
+
+# def test_attributes(gpvolve_base):
+#     assert isinstance()
